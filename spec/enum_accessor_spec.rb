fdd571dec374a70acd4d44a836220afe74eed5c0
Kenn Ejima <kenn.ejima@gmail.com>
2014-08-20 15:18:34 -0700

    RSpec 3

--- spec/enum_accessor_spec.rb
+++ spec/enum_accessor_spec.rb
@@ -16,57 +16,57 @@ describe EnumAccessor do
   end
 
   it 'adds checker' do
-    @user.gender_female?.should == true
-    @user.gender_male?.should == false
+    expect(@user.gender_female?).to eq(true)
+    expect(@user.gender_male?).to eq(false)
   end
 
   it 'adds getter' do
-    @user.gender.should == :female
+    expect(@user.gender).to eq(:female)
   end
 
   it 'adds setter' do
     @user.gender = :male
-    @user.gender_male?.should == true
+    expect(@user.gender_male?).to eq(true)
   end
 
   it 'adds raw value getter' do
-    @user.gender_raw.should == 0
+    expect(@user.gender_raw).to eq(0)
   end
 
   it 'adds raw value setter' do
     @user.gender_raw = 1
-    @user.gender_male?.should == true
+    expect(@user.gender_male?).to eq(true)
   end
 
   it 'adds humanized methods' do
     I18n.locale = :ja
-    User.human_attribute_name(:gender).should == '性別'
-    @user.human_gender.should == '女'
-    User.human_genders(:female).should == '女'
-    User.human_genders.should == { :female => '女', :male => '男' }
+    expect(User.human_attribute_name(:gender)).to eq('性別')
+    expect(@user.human_gender).to eq('女')
+    expect(User.human_genders(:female)).to eq('女')
+    expect(User.human_genders).to eq({ :female => '女', :male => '男' })
 
     I18n.locale = :en
-    User.human_attribute_name(:gender).should == 'Gender'
-    @user.human_gender.should == 'Female'
-    User.human_genders(:female).should == 'Female'
-    User.human_genders.should == { :female => 'Female', :male => 'Male' }
+    expect(User.human_attribute_name(:gender)).to eq('Gender')
+    expect(@user.human_gender).to eq('Female')
+    expect(User.human_genders(:female)).to eq('Female')
+    expect(User.human_genders).to eq({ :female => 'Female', :male => 'Male' })
   end
 
   it 'defines internal constant' do
-    User::GENDERS.should == { "female" => 0, "male" => 1 }
+    expect(User::GENDERS).to eq({ "female" => 0, "male" => 1 })
   end
 
   it 'adds class methods' do
-    User.genders.should == { :female => 0, :male => 1 }
-    User.genders[:female].should == 0
-    User.genders(:female).should == 0
+    expect(User.genders).to eq({ :female => 0, :male => 1 })
+    expect(User.genders[:female]).to eq(0)
+    expect(User.genders(:female)).to eq(0)
   end
 
   it 'adds validation' do
     @user.gender = 'bogus'
-    @user.valid?.should be_false
+    expect(@user.valid?).to be_falsey
 
     @user.gender = 'male'
-    @user.valid?.should be_true
+    expect(@user.valid?).to be_truthy
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,6 @@ RSpec.configure do |config|
   ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
 
   # Load translation
+  I18n.enforce_available_locales = true
   I18n.load_path << File.join(File.dirname(__FILE__), 'locales.yml')
 end
